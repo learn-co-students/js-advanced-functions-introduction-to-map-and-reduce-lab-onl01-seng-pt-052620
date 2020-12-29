590cf8937b010f0b7ec5cdf2ae19abd86c746de7
iamcid <iamcid17@gmail.com>
2020-12-29 01:23:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+    let newArray = []
+    sourceArray.forEach(item => {
+        let i = item * -1
+        newArray.push(i)
+    });
+    return newArray 
+}
+
+function mapToNoChange(sourceArray) {
+    return sourceArray;
+}
+
+function mapToDouble(sourceArray) {
+    let newArray = []
+    sourceArray.forEach(item => {
+        let i = item * 2
+        newArray.push(i)
+    });
+    return newArray;
+}
+
+function mapToSquare(sourceArray) {
+    let newArray = []
+    sourceArray.forEach(item => {
+        let i = item ** 2
+        newArray.push(i)
+    });
+    return newArray;
+}
+
+
+function reduceToTotal(sourceArray, startingPoint = 0) {
+    let currentTotal = startingPoint;
+    sourceArray.forEach(item => {
+        currentTotal += item
+    });
+    return currentTotal;
+}
+
+function reduceToAllTrue(sourceArray) {
+    for (let i = 0; i < sourceArray.length; i++) {
+        if (!sourceArray[i]) return false
+    }
+    return true;
+}
+
+function reduceToAnyTrue(sourceArray) {
+    for (let i = 0; i < sourceArray.length; i++) {
+        if (sourceArray[i]) return true
+    }
+    return false;
+}
